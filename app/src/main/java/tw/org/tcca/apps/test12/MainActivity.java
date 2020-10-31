7b560745293c77eb8891e30dcf4a9252f6ab39fe
bradchao <brad@brad.tw>
2020-10-31 10:00:57 +0800

    4

--- app/src/main/java/tw/org/tcca/apps/test12/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/test12/MainActivity.java
@@ -1,13 +1,18 @@
 package tw.org.tcca.apps.test12;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.werb.pickphotoview.PickPhotoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,6 +54,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void fetchPhoto(View view) {
+        new PickPhotoView.Builder(this)
+                .setPickPhotoSize(3)                  // select image size
+                .setClickSelectable(false)             // click one image immediately close and return image
+                .setShowCamera(true)                  // is show camera
+                .setSpanCount(3)                      // span count
+                .setLightStatusBar(true)              // lightStatusBar used in Android M or higher
+                .setShowGif(false)                    // is show gif
+                .start();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.v("bradlog", "OK");
     }
 
     public void upload(View view) {
